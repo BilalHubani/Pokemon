a2866b802a991c2a430842eeeca625e10478bac7
BilalHubani <bilal.hubani@gmail.com>
2016-10-03 17:30:57 +0200

    actu

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,10 @@
 public class Main {
     public static void main(String[] args) {
         Pokemon chandelure = new Pokemon("Chandelure", 152, 79, 147);
+        Pokemon charizard = new Pokemon("Charizard", 179, 82, 168);
+        Pokemon vaporeon = new Pokemon("Vaporeon", 95, 143, 210);
+        Pokemon lapras = new Pokemon("Lapras", 79, 132, 217);
+        Pokemon breloom = new Pokemon("Breloom", 148, 50, 112);
+        Pokemon bulbasaur = new Pokemon("Bulbasaur", 52, 63, 91);
     }
 }
---
+++ src/capturable.java
@@ -0,0 +1,6 @@
+/**
+ * Created by dam on 3/10/16.
+ */
+public interface capturable {
+    public String capturar();
+}
